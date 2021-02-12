1caa4344bbf957ad40d00488776f4353cbaf8226
Gabriel Almeida <galmeida9@gmail.com>
2021-02-12 18:17:59 +0000

    electron react app with jpn word search

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import SearchAppBar from './components/SearchAppBar';
+import InteractiveList from './components/InteractiveList'
+import Toolbar from '@material-ui/core/Toolbar';
+
+class App extends React.Component {
+  state = {
+    searchValue: null
+  }
+
+  constructor(props: any) {
+    super(props)
+    this.saveItemToSearch = this.saveItemToSearch.bind(this)
+  }
+  
+  saveItemToSearch(item) {
+    this.setState({searchValue: item})
+    // console.log(item)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <SearchAppBar SearchData={this.saveItemToSearch}/>
+        <Toolbar />
+        <InteractiveList itemToSearch={this.state.searchValue} />
+      </div>
+    );
+  }
+}
+
+export default App;
